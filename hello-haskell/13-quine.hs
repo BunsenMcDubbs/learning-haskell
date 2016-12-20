a6468df424e90c11e38ba5b27f67529686423350
Andrew Dai <theman@andrewdai.co>
2016-12-20 18:14:21 -0500

    quine in haskell!

---
+++ hello-haskell/13-quine.hs
@@ -0,0 +1,6 @@
+import System.IO
+
+main = do
+    let p = ["import System.IO\n\n","main = do\n","    let p = "]
+    let q = "mapM_ putStr (p ++ [show p, \"\\n    let q = \" ++ show q, \"\\n    \" ++ q])"
+    mapM_ putStr (p ++ [show p, "\n    let q = " ++ show q, "\n    " ++ q])

552b98ac7eaaaeb0148d9a96a30f229dd3ec4faf
Andrew Dai <theman@andrewdai.co>
2016-12-20 18:21:10 -0500

    add newline to the end of the quine

--- hello-haskell/13-quine.hs
+++ hello-haskell/13-quine.hs
@@ -2,5 +2,5 @@ import System.IO
 
 main = do
     let p = ["import System.IO\n\n","main = do\n","    let p = "]
-    let q = "mapM_ putStr (p ++ [show p, \"\\n    let q = \" ++ show q, \"\\n    \" ++ q])"
-    mapM_ putStr (p ++ [show p, "\n    let q = " ++ show q, "\n    " ++ q])
+    let q = "mapM_ putStr (p ++ [show p, \"\\n    let q = \" ++ show q, \"\\n    \" ++ q, \"\\n\"])"
+    mapM_ putStr (p ++ [show p, "\n    let q = " ++ show q, "\n    " ++ q, "\n"])
